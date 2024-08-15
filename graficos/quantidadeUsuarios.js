9a68cccace15a8d5160a89457b72d0a1e1d207ba
silviosnjr <silviosnjr@gmail.com>
2024-08-15 10:31:42 -0300

    Construíndo gráficos

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,23 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const rest = await fetch(url)
+    const dados = await rest.json()
+
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios= Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar'
+        }
+    ]
+    
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuarios()
