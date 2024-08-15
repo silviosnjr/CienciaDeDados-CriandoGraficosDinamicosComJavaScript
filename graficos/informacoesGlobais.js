74d5e06be9380a69ed20c063d6bf27bc3405dff5
silviosnjr <silviosnjr@gmail.com>
2024-08-15 08:46:29 -0300

    código fonte da aula 07

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,12 +3,19 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e 
-    que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e 
-    passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-    console.log(paragrafo)
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e 
+    que aproximadamente <span>${pessoasConectadas}</span> bilhões estão conectadas em alguma rede social e 
+    passam em média <span>${horas} horas</span> e <span>${minutos} minutos
+    </span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span>
+    pessoas conectadas em alguma rede social. `
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
