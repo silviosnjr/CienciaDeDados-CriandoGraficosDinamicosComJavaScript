b20ed9997f0fdf2d685c71214cc33b72eb188af1
silviosnjr <silviosnjr@gmail.com>
2024-08-15 10:57:34 -0300

    Utilizando API

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,9 +1,9 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url);
-    const dados = await res.json();
-    console.log(dados);
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)
 }
 
-vizualizarInformacoesGlobais();
+vizualizarInformacoesGlobais()
