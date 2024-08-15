aef6bf4e70b7a791fb8e6cde1e4b3bec6f33be15
silviosnjr <silviosnjr@gmail.com>
2024-08-15 08:26:29 -0300

    código fonte da aula 05

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,9 +1,17 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url);
-    const dados = await res.json();
-    console.log(dados);
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e 
+    que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e 
+    passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
 }
 
 vizualizarInformacoesGlobais();
