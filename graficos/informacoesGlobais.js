3ee3ec359bc52f4494a9f0160c9cea37e7701709
silviosnjr <silviosnjr@gmail.com>
2024-10-07 11:56:55 -0300

    código da reunião 04

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,13 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
-    console.log(dados);
-    console.log("Temos "+dados.total_pessoas_mundo+" de pessoas no mundo.");
-    console.log("E "+dados.total_pessoas_conectadas+" estão conectadas.");
-    console.log("Ficando em média "+dados+" horas por dia na internet.");
-
+    //console.log(dados)
+    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
 }
 
 vizualizarInformacoesGlobais();
