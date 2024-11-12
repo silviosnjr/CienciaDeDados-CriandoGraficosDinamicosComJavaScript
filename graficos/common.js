2b0508c8b17097f5c11c10a25b60cf537834ab06
silviosnjr <silviosnjr@gmail.com>
2024-11-12 18:50:44 -0300

    Códigos fontes da reunião 8

---
+++ graficos/common.js
@@ -0,0 +1,11 @@
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+}
+ 
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
+
+export {getCSS, tickConfig}
--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,23 +1,59 @@
-async function quantidadesUsuarios(){
+import { getCSS, tickConfig } from "./common.js"
+
+async function quantidadesUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    
+
     const nomeDasRedes = Object.keys(dados)
     const quantidadesUsuarios = Object.values(dados)
 
     const data = [
         {
-            x : nomeDasRedes,
-            y : quantidadesUsuarios,
-            type: 'bar'
+            x: nomeDasRedes,
+            y: quantidadesUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
         }
     ]
-    
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: "Redes sociais com mais usuários no mundo",
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30,
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: "Nome das redes sociais",
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: "Bilhoes de usuários ativos",
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        }
+    }
+
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadesUsuarios();
